eeaafdec2425961898c72a090d2463c7e964f166
stardain <crimsonrain@internet.ru>
2025-11-08 21:22:02 +1100

    started setting up django structure...

---
+++ manage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
+
+# получает тайминги 
+# получает весь анализ
+# сохраняет путешествие в бд
+# выдаёт результат итоговой странице
+
+# классы для каждого региона
+# celery + redis для коммуникации аналитиков
+# unittest для тестирования каждого модуля
+
+
+import os
+import sys
+import psycorg2
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
